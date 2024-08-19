4d206aeddb9ae4b36a98b5f54aab8c821a3c698c
Samuriot <92127962+Samuriot@users.noreply.github.com>
2024-08-18 21:20:44 -0700

    added squares on home page

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,10 +1,12 @@
 import {View, Text, FlatList } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import ColorList from '@/components/ColorList'
 
 const Home = () => {
     return (
         <SafeAreaView>
+            <ColorList color = "#DECEF4"/>
         </SafeAreaView>
     )
 }
