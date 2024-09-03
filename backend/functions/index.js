ceac9e139ff75e81b04fc49019c37c05bbf07b64
Samuriot <jomi.g.ruiz@gmail.com>
2024-09-02 23:56:39 -0700

    added create functionality

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -6,10 +6,12 @@ const serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://handsup-eb897-default-rtdb.firebaseio.com",
 });
 
 const express = require("express");
 const cors = require("cors");
+const db = admin.firestore();
 
 const app = express();
 app.use(cors({origin: true}));
@@ -21,6 +23,24 @@ app.get("/", (req, res) => {
 
 // Create
 // Post
+app.post("/api/create", (req, res) => {
+    (async () => {
+        try {
+            await db.collection("concerts").doc("/" + req.body.id + "/")
+            .create({
+                artist: req.body.artist,
+                data: req.body.data,
+                location: req.body.location,
+                picture: req.body.picture,
+            });
+            return res.status(200).send();
+        }
+        catch (error) {
+            console.log(error);
+            return res.status(500).send(error);
+        }
+    })();
+});
 
 // Read
 // Get
